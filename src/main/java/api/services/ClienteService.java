a74004e8fb3b2d0f4ed123d1e8e29e6c1414e949
Juan Molano <molano.juan.2629@eam.edu.co>
2025-04-06 17:30:48 -0500

    ELIMINADO repository de usuarios del service de clientes, reemplazado por un service de usuarios para no acceder al repository de usuarios directamente.

--- src/main/java/api/services/ClienteService.java
+++ src/main/java/api/services/ClienteService.java
@@ -5,8 +5,8 @@
 package api.services;
 
 import api.models.Cliente;
+import api.models.Usuario;
 import api.repositories.ClienteRepository;
-import api.repositories.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,16 +18,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService {
 
-    private final UsuarioRepository usuarioRepository;
+    private final UsuarioService usuarioService;
     private final ClienteRepository clienteRepository;
 
     @Autowired
-    public ClienteService(UsuarioRepository usuarioRepository, ClienteRepository clienteRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public ClienteService(UsuarioService usuarioService, ClienteRepository clienteRepository) {
+        this.usuarioService = usuarioService;
         this.clienteRepository = clienteRepository;
     }
 
     public List<Cliente> getAllClientes() {
-        return clienteRepository.getAllClientes(usuarioRepository.getAllUsuarios());
+        List<Usuario> usuarios = usuarioService.getAllUsuarios();
+        return clienteRepository.getAllClientes(usuarios);
     }
 }
